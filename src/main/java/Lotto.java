1ffc5c9b469254eb521cb62b6b23c1423da9ba8d
Leehaewon <leehaewon68@gmail.com>
2019-01-06 16:16:18 +0900

    change lotto class - semicolon delete

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class Lotto {
 
-    private ArrayList<Integer> lotto;
+    private ArrayList<Integer> lotto = new ArrayList<Integer>();
 
-    public ArrayList<Integer> getLotto() { return this.lotto; };
-    public void setLotto(ArrayList<Integer> lotto) { this.lotto = lotto; };
+    public ArrayList<Integer> getLotto() { return this.lotto; }
+    public void setLotto(ArrayList<Integer> lotto) { this.lotto = lotto; }
 }
