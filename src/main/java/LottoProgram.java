7e9d425e91826c095d8cfe73e57ff8f8943f0320
Leehaewon <leehaewon68@gmail.com>
2019-01-06 16:25:25 +0900

    add LottoProgram - ing...

--- src/main/java/LottoProgram.java
+++ src/main/java/LottoProgram.java
@@ -1,2 +1,8 @@
 public class LottoProgram {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        inputView.setBuyingLottoAmount();
+        inputView.printBuyingLottoNum();
+        inputView.printBuyingLottoList();
+    }
 }
