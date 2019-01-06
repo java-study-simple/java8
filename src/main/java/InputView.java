c3c46aade9d372d582622fb6745652a0190dc01f
Leehaewon <leehaewon68@gmail.com>
2019-01-06 16:24:17 +0900

    change InputView - method printBuyingLottoNum, printBuyingLottoList

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -36,14 +36,15 @@ public class InputView {
     public void printBuyingLottoNum() {
         buyingLottoNum = getBuyingLottoNum();
         System.out.println(buyingLottoNum+"개를 구매했습니다.");
+        setBuyingLottoList(buyingLottoNum);
     }
 
-    public void printBuyingLottoList(ArrayList<Lotto> lottolist) {
-        for(int i=0;i<lottolist.size();i++) {
+    public void printBuyingLottoList() {
+        for(int i=0;i<buyingLottoList.size();i++) {
             System.out.print("[");
-            for(int j=0;j<lottolist.get(i).getLotto().size();j++) {
-                System.out.print(lottolist.get(i).getLotto().get(j));
-                if(j!=lottolist.get(i).getLotto().size()-1){
+            for(int j=0;j<buyingLottoList.get(i).getLotto().size();j++) {
+                System.out.print(buyingLottoList.get(i).getLotto().get(j));
+                if(j!=buyingLottoList.get(i).getLotto().size()-1){
                     System.out.print(", ");
                 }
             }
