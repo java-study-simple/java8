89e9a1d9cc17e80e3ae678f6d4d76bf27bd09340
Leehaewon <leehaewon68@gmail.com>
2019-01-06 15:30:54 +0900

    add lotto class

---
+++ src/main/java/Lotto.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Lotto {
+
+    private ArrayList<Integer> lotto;
+
+    public ArrayList<Integer> getLotto() { return this.lotto; };
+    public void setLotto(ArrayList<Integer> lotto) { this.lotto = lotto; };
+}
